7f181a4a84e2b9991819cae5af10ccfd92bd8700
Marcio Katsumi Yamashita <marciokatsumiyamashita@gmail.com>
2017-04-03 16:31:52 -0300

    fix compress_view

--- Decompress.py
+++ Decompress.py
@@ -1,8 +1,8 @@
-import zlib
 import os
+import zlib
 import sublime
-import sublime_plugin
 import sublime_api
+import sublime_plugin
 
 cache_path = sublime_api.cache_path()
 path_compressed = cache_path[:-5] + 'OCN_compressed\\'
@@ -11,10 +11,9 @@ path_uncompressed = cache_path[:-5] + 'OCN_uncompressed\\'
 class CompressViewCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
-        filename = self.view.name()
+        filename = self.view.name() if self.view.name() != '' else self.view.file_name()
         name = os.path.basename(filename)
-        self.view.set_encoding('Western (Windows 1252)')
-        file = sublime_api.view_cached_substr(self.view.view_id, 
+        file = sublime_api.view_cached_substr(self.view.view_id,
                                               0, self.view.size())
         file = file.replace('\n', '\r\n')
         file = file.encode('cp1252', 'ignore')
@@ -23,12 +22,17 @@ class CompressViewCommand(sublime_plugin.TextCommand):
         textcompre = zlib.compress(file)
         filegz.write(textcompre)
         filegz.close()
-        self.view.erase(edit, sublime.Region(0, self.view.size()))
-        self.view.close()
+        if self.view.name() != '':
+            self.view.erase(edit, sublime.Region(0, self.view.size()))
+            self.view.close()
+        else:
+            self.view.run_command('save')
+            self.view.close()
 
 class DecompressFilesCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
+        filename = view.file_name()
         FileNames = askopenfilenames(filetypes=[("ADC", "*.adc"),
                                      ("FSI", "*.fsi"),
                                      ("MIR", "*.mir"),
